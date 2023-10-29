5735f0a7dc905a4e9ff61c935a6a82d9ffa958a3
Shaikhismail087 <shaikhismail0876262@gmail.com>
2023-10-30 00:53:00 +0530

    Adding Okta & spring security

--- src/main/java/com/lswd/hotel/HotelServiceApplication.java
+++ src/main/java/com/lswd/hotel/HotelServiceApplication.java
@@ -3,7 +3,7 @@ package com.lswd.hotel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.springframework.security.oauth2.jwt")
 public class HotelServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/lswd/hotel/controllers/HotelController.java
+++ src/main/java/com/lswd/hotel/controllers/HotelController.java
@@ -5,6 +5,7 @@ import com.lswd.hotel.services.HotelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,19 +16,23 @@ public class HotelController {
 
     @Autowired
     private HotelService hotelService;
+
     //create
+    @PreAuthorize("hasAuthority('Admin')")
     @PostMapping
     public ResponseEntity<Hotel> createHotel(@RequestBody Hotel hotel){
         return ResponseEntity.status(HttpStatus.CREATED).body(hotelService.create(hotel));
     }
 
     //get single
+    @PreAuthorize("hasAuthority('SCOPE_internal')")
     @GetMapping("/{hotelId}")
     public ResponseEntity<Hotel> getSingle(@PathVariable String hotelId){
         return ResponseEntity.status(HttpStatus.OK).body(hotelService.get(hotelId));
     }
 
     //get all
+    @PreAuthorize("hasAuthority('SCOPE_internal') || hasAuthority('Admin')")
     @GetMapping
     public ResponseEntity<List<Hotel>> getAll(){
         return ResponseEntity.ok(hotelService.getAll());
